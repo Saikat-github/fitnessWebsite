1486583f4c37239ef4a6d55c71b6065307e2b69d
Saikat <saikatsaha.rph@gmail.com>
2025-04-04 14:47:08 +0530

    changed button

--- src/components/Button1.jsx
+++ src/components/Button1.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Button1 = ({children, type="submit", className="", ...props}) => {
     return (
-        <button type={type} className={`${className} px-6 py-2 rounded-full text-white hover:scale-110 transition-all duration-300 bg-gradient-to-r from-purple-600 via-pink-600 to-red-600`} {...props}>
+        <button type={type} className={`${className} px-6 py-2 rounded-full  hover:scale-110 transition-all duration-300 border-2`} {...props}>
             {children}
         </button>
     )
